c4017aa3c94d1e2b2b498813a025fe452d6bd3c4
Denzel <denzelk741@gmail.com>
2024-11-14 20:21:40 +0300

    Host  added

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ setupDatabase()
     // Server setup
     const PORT = process.env.PORT || 8080;
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   })
